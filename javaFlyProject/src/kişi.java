45dd9db8b5e985b28886299bf045e351b9e5274b
Dilan <Dilangul035@gmail.com>
2023-01-21 19:34:27 +0300

    week4

---
+++ javaFlyProject/src/kişi.java
@@ -0,0 +1,48 @@
+
+  package havalimani.odev;
+  import java.util.Date;
+
+public class Kisi {
+    private String kisiTc;
+    private String adSoyad;
+    private String adres;
+    private Date dogumTarihi;
+
+    public String getKisiTc() {
+        return kisiTc;
+    }
+
+    public void setKisiTc(String kisiTc) {
+        this.kisiTc = kisiTc;
+    }
+
+    public String getAdSoyad() {
+        return adSoyad;
+    }
+
+    public void setAdSoyad(String adSoyad) {
+        this.adSoyad = adSoyad;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public Date getDogumTarihi() {
+        return dogumTarihi;
+    }
+
+    public void setDogumTarihi(Date dogumTarihi) {
+        this.dogumTarihi = dogumTarihi;
+
+    }
+
+
+
+
+
+
