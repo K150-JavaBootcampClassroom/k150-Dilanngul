7f4350a91d40371f15019f6cf1b640230a241ab5
Dilan <Dilangul035@gmail.com>
2023-01-24 20:10:36 +0300

    Add files via upload

---
+++ javaFlyProject/Main.java
@@ -0,0 +1,49 @@
+
+    package Havalimani.Odev;
+    import java.util.List;
+
+    public class Havalimani {
+        private int HavalimaniNo;
+        private String HavalimaniAd;
+        private String Ulke;
+        private String Şehir;
+
+        public int getHavalimaniNo() {
+            return havalimaniNo;
+        }
+
+        public void setHavalimaniNo(int havalimaniNo) {
+            this.havalimaniNo = havalimaniNo;
+        }
+
+        public String getHavalimaniAd() {
+            return havalimaniAd;
+        }
+
+        public void setHavalimaniAd(String havalimaniAd) {
+            this.havalimaniAd = havalimaniAd;
+        }
+
+        public String getUlke() {
+            return ulke;
+        }
+
+        public void setUlke(String ulke) {
+            this.ulke = ulke;
+        }
+
+        public String getSehir() {
+            return sehir;
+        }
+
+        public void setSehir(String sehir) {
+            this.sehir = sehir;
+        }
+
+        public static  List<Ucus> getirUcuslar() throws IOException {
+            return Yonetici.getirUcus();
+
+        }
+
+
+    }
---
+++ javaFlyProject/koltuk.java
@@ -0,0 +1,30 @@
+
+package havalimani.odev;
+
+
+public class Koltuk {
+    private String koltukNo;
+    private KoltukSinifi sinif;
+
+    public KoltukSinifi getSinif() {
+        return sinif;
+    }
+
+    public void setSinif(KoltukSinifi sinif) {
+        this.sinif = sinif;
+    }
+    public void setKoltukNo( String koltukNo) {
+        this.koltukNo = koltukNo;
+    }
+    public String getKoltukNo() {
+        return koltukNo;
+    }
+
+    public KoltukSinifi getirKoltukSinifi() {
+        return sinif;
+    }
+
+
+
+
+}
